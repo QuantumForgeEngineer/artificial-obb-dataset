e88a8203491a32ec6002ee749f5b71b9a70fae54
QuantumForgeEngineer <166127086+QuantumForgeEngineer@users.noreply.github.com>
2024-04-17 14:40:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from utils import remove_all_files
+from utils import generate_images
+
+# Parameters
+num_images = 50000  # Total number of images to generate
+image_size = 512  # Size of each image
+
+writing_path = "/path/where/to/write/data"
+
+# Empty folders
+if os.path.exists(writing_path):
+  for fol in [os.path.join(writing_path, "images"), os.path.join(writing_path, "labels")]:
+      for ds in ['train', 'val', 'test']:
+          remove_all_files(os.path.join(fol, ds))
+else:
+  os.makedirs(os.path.join(writing_path, "images"))
+  os.makedirs(os.path.join(writing_path, "labels"))
+  for fol in [os.path.join(writing_path, "images"), os.path.join(writing_path, "labels")]:
+      for ds in ['train', 'val', 'test']:
+          os.makedirs(os.path.join(fol, ds))
+        
+# Generate images and labels
+generate_images(num_images, image_size)
